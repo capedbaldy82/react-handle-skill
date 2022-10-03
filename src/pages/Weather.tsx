3edac88311d785149b9d3d12d32d21c57121d3ce
chango <kimtothechang@gmail.com>
2022-10-04 01:06:31 +0900

    test: axios

--- src/pages/Weather.tsx
+++ src/pages/Weather.tsx
@@ -1,6 +1,27 @@
+import { useState } from "react";
+import axios from 'axios';
+
 const Weather = () => {
+    const [data, setData] = useState([]);
+
+    const onClick = async () => {
+        try {
+            const response = await axios.get('https://openmarket.weniv.co.kr/products/');
+            setData(response.data.results);
+        } catch (e) {
+            console.log(e);
+        }
+    }
     return <h1>
         Weather
+        <button onClick={onClick}>GET</button>
+        <ul>
+            {data && data.map((item: { product_name: string }, i) => (
+                <li key={i}>
+                    {item.product_name}
+                </li>
+            ))}
+        </ul>
     </h1>
 }
 
